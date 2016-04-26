70908f6ce33831685bf4119e7ebb919fa87409ee
Tom Baro <tom@klandestino.se>
2016-04-26 11:36:20 +0200

    Visa hur alla har tipsat

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var tips = require('../shared/tips/score.js');
+var score = require('../shared/tips/score.js');
+var tips = require('../shared/tips/tips.json');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
   	title: 'Tippning Allsvenskan 2016',
-  	score: tips.score()
+	score: score.score(),
+	tips: tips
   });
 });
 
